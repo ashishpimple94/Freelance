d9bdb63bfa1c3eab45f6569a7c27f95832ec4526
Ashish <ashish@stradit.com>
2026-03-13 22:37:56 +0530

    Fixed server binding for Hostinger

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ app.use('/api/tasks', require('./routes/tasks'));
 app.use('/api/payments', require('./routes/payments'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
